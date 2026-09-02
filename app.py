ae95f94e35c8765cc2bfbd3e58ef5d36f85f370f
Gabriel Mukamba <gabrielmukamba7@gmail.com>
2026-09-02 19:55:40 +0300

    Update server port binding for Render routing grid

--- app.py
+++ app.py
@@ -50,5 +50,6 @@ def translate():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    # Bind explicitly to port 10000 or the dynamically assigned system environment port
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
